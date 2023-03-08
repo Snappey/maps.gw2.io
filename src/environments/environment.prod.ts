2f1215765c205a9fd9ec5188d493eb3429668710
Jon Nuttall <jon.nuttall@nomuda.com>
2023-03-08 20:57:53 +0000

    Updated prod to use post.gw2.io/auth instead of DO serverless functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   liveMarkers: {
-    authUrl: "https://faas-lon1-917a94a7.doserverless.co/api/v1/web/fn-77f98c90-2b96-4a2a-97bc-66538b4d5e47/post.gw2.io/jwt-authentication"
+    authUrl: "https://post.gw2.io/auth"
   }
 };
